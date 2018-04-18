19d8a765899a5927e8a5569a7d5ea6aa74fa84b3
Javier <valentincolen@outlook.com>
2018-04-18 17:23:34 +0200

    Modified in order to add user to notify method

--- notification/telegram.py
+++ notification/telegram.py
@@ -8,12 +8,13 @@ class TelegramNotification(Notification):
         Notification.__init__(self, token, chat_id, logger)
         self.bot = telepot.Bot(self.token)
 
-
-    def notify(self, message):
-        if (len(self.chat_id_list) == 1):
-            self.logger.debug('Message {} send to user {}'.format(message, self.chat_id))
-            self.bot.sendMessage(self.chat_id, message)
+    def notify(self, user, message):
+        self.bot.sendMessage(user, message)
+        self.logger.debug('Message {} send to user {}'.format(message, user))
 
     def broadcast(self, message):
-        for chat_id in self.chat_id_list:
-            self.bot.sendMessage(chat_id, message)
+        self.logger.debug("Initialized Broadcast")
+        for identification in self.chat_id_list:
+            self.bot.sendMessage(identification, message)
+            self.logger.debug("Message {} send to {}".format(message, identification))
+        self.logger.debug("Finished broadcasting")
